407af189b78e64c1af487f6ff854bdc1cb92f63d
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2020-08-06 10:14:04 +1200

    random stuff

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// Set up 
+function setup (){
+    createCanvas
+(800, 600);
+}
+//Trying to make a damn square
+function draw (){
+    square(200, 200, 200, 200)
+    stroke(200, 200, 200, 200)
+    stroke weight(6)
+}
